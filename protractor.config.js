85b9e00562bbdf43418b33f7c120520fca922255
Shantur Egor <eshantur@exadel.by>
2019-01-29 12:34:46 +0300

    Fixed allure report

--- protractor.config.js
+++ protractor.config.js
@@ -24,20 +24,28 @@ exports.config = {
     plugins: [
     ],
 
-    reporters: ['allure'],
-    reporterOptions: {
-        allure: {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: true,
-            useCucumberStepReporter: false
-        }
-    },
-
     onPrepare: async function () {
         const session = await global.browser.getSession();
         global.browser.params.sessionId = await session.getId();
         browser.ignoreSynchronization = true;
+        
+        let originalAddExpectationResult = jasmine.Spec.prototype.addExpectationResult;
+        jasmine.Spec.prototype.addExpectationResult = function () {
+            if (!arguments[0]) {
+                browser.takeScreenshot().then(function (png) {
+                    allure.createAttachment('Screenshot', function () {
+                        return new Buffer(png, 'base64')
+                    }, 'image/png')();
+                })
+            }
+            return originalAddExpectationResult.apply(this, arguments);
+        };
+
+        const AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+
         return global.browser.getProcessedConfig().then(function (config) {
         });
     },
